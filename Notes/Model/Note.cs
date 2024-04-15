f55cfbf9c4daa11c6ed8d28a392e40f51813688f
radoslav hristov <bmwto@outlook.com>
2024-04-15 21:28:13 +0300

    Add project files.

---
+++ Notes/Model/Note.cs
@@ -0,0 +1,9 @@
+﻿namespace Notes.Model
+{
+    public class Note
+    {
+        public string Filename { get; set; } = null!;
+        public string Text { get; set; } = null!;
+        public DateTime Date { get; set; }
+    }
+}
---
+++ Notes/Views/NotePage.xaml.cs
@@ -0,0 +1,56 @@
+namespace Notes.Views;
+
+[QueryProperty(nameof(ItemId), nameof(ItemId))]
+public partial class NotePage : ContentPage
+{
+    string _fileName = Path.Combine(FileSystem.AppDataDirectory, "notes.txt");
+    public NotePage()
+    {
+
+        InitializeComponent();
+
+        string appDataPath = FileSystem.AppDataDirectory;
+        string randomFileName = $"{Path.GetRandomFileName()}.notes.txt";
+
+        LoadNote(Path.Combine(appDataPath, randomFileName));
+    }
+
+    public string ItemId
+    {
+        set { LoadNote(value); }
+    }
+
+    private void LoadNote(string fileName)
+    {
+        Model.Note noteModel = new Model.Note();
+        noteModel.Filename = fileName;
+
+        if (File.Exists(fileName))
+        {
+            noteModel.Date = File.GetCreationTime(fileName);
+            noteModel.Text = File.ReadAllText(fileName);
+        }
+
+        BindingContext = noteModel;
+    }
+
+    private async void SaveButton_Clicked(object sender, EventArgs e)
+    {
+        if (BindingContext is Model.Note note)
+            File.WriteAllText(note.Filename, note.Text);
+
+        await Shell.Current.GoToAsync("..");
+    }
+
+    private async void DeleteButton_Clicked(object sender, EventArgs e)
+    {
+        if (BindingContext is Model.Note note)
+        {
+            // Delete the file.
+            if (File.Exists(note.Filename))
+                File.Delete(note.Filename);
+        }
+
+        await Shell.Current.GoToAsync("..");
+    }
+}
